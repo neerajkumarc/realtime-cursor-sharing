4cfee40593723428a2715c709f1faf3485e7fa03
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-02-02 22:52:51 +0530

    add html page

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express")
+const socketio = require("socket.io")
+const http = require("http")
 const PORT = process.env.PORT | 3001
 const app = express()
+const server = http.createServer(app)
+const io = new socketio.Server(server)
 
-app.listen(PORT,()=>{
+app.use(express.static("public"))
+
+server.listen(PORT,()=>{
     console.log("server started...")
 })
